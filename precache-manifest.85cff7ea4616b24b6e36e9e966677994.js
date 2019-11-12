396215df3c03d11e229ab8c81fb5221168b82a8d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-12 14:37:05 +0000

    Deploy Ridikon/react-menu-list to github.com/Ridikon/react-menu-list.git:gh-pages

---
+++ precache-manifest.85cff7ea4616b24b6e36e9e966677994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4a59dbb7c726b7fe60ad654aad7283",
+    "url": "/react-menu-list/index.html"
+  },
+  {
+    "revision": "380ae5fd885c6a7657f1",
+    "url": "/react-menu-list/static/css/main.aa9c644d.chunk.css"
+  },
+  {
+    "revision": "74a247effbe78acdd2b1",
+    "url": "/react-menu-list/static/js/2.ce954f07.chunk.js"
+  },
+  {
+    "revision": "380ae5fd885c6a7657f1",
+    "url": "/react-menu-list/static/js/main.e2847370.chunk.js"
+  },
+  {
+    "revision": "c7970842ff92e666bd66",
+    "url": "/react-menu-list/static/js/runtime-main.fa81dc9d.js"
+  }
+]);
